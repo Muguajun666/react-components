b01631dee883570893bd02fae7820ec33b6394c0
liushuli <liushulialen@qq.com>
2024-11-28 23:55:31 +0800

    feat: 完成主题切换 分享 下载

--- react-playground/src/ReactPlayground/PlaygroundContext.tsx
+++ react-playground/src/ReactPlayground/PlaygroundContext.tsx
@@ -1,5 +1,5 @@
-import React, { PropsWithChildren, createContext, useState } from "react";
-import { fileName2Language } from "./utils";
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
+import { compress, fileName2Language, uncompress } from "./utils";
 import { initFiles } from "./files";
 
 export interface File {
@@ -12,9 +12,13 @@ export interface Files {
   [key: string]: File;
 }
 
+export type Theme = "light" | "dark";
+
 export interface PlaygroundContext {
   files: Files;
   selectedFileName: string;
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
   setSelectedFileName: (fileName: string) => void;
   setFiles: (files: Files) => void;
   addFile: (fileName: string) => void;
@@ -26,11 +30,23 @@ export const PlaygroundContext = createContext<PlaygroundContext>({
   selectedFileName: "App.tsx",
 } as PlaygroundContext);
 
+const getFilesFromUrl = () => {
+  let files: Files | undefined;
+  try {
+    const hash = uncompress(window.location.hash.slice(1));
+    files = JSON.parse(hash);
+  } catch (e) {
+    console.error(e);
+  }
+  return files;
+};
+
 export const PlaygroundProvider = (props: PropsWithChildren) => {
   const { children } = props;
 
-  const [files, setFiles] = useState<Files>(initFiles);
+  const [files, setFiles] = useState<Files>(getFilesFromUrl() || initFiles);
   const [selectedFileName, setSelectedFileName] = useState("App.tsx");
+  const [theme, setTheme] = useState<Theme>("light");
 
   const addFile = (fileName: string) => {
     files[fileName] = {
@@ -47,26 +63,38 @@ export const PlaygroundProvider = (props: PropsWithChildren) => {
   };
 
   const updateFileName = (oldFieldName: string, newFieldName: string) => {
-    if (!files[oldFieldName] || newFieldName === undefined || newFieldName === null) return
-    const {[oldFieldName]: value, ...rest} = files
+    if (
+      !files[oldFieldName] ||
+      newFieldName === undefined ||
+      newFieldName === null
+    )
+      return;
+    const { [oldFieldName]: value, ...rest } = files;
     const newFile = {
       [newFieldName]: {
         ...value,
         language: fileName2Language(newFieldName),
         name: newFieldName,
-      }
-    }
+      },
+    };
     setFiles({
       ...rest,
       ...newFile,
-    })
+    });
   };
+
+  useEffect(() => {
+    const hash = compress(JSON.stringify(files));
+    window.location.hash = hash;
+  }, [files]);
 
   return (
     <PlaygroundContext.Provider
       value={{
         files,
         selectedFileName,
+        theme,
+        setTheme,
         setSelectedFileName,
         setFiles,
         addFile,
--- react-playground/src/ReactPlayground/utils.ts
+++ react-playground/src/ReactPlayground/utils.ts
@@ -1,3 +1,8 @@
+import { strFromU8, strToU8, unzlibSync, zlibSync } from "fflate";
+import { Files } from "./PlaygroundContext";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+
 export const fileName2Language = (fileName: string) => {
   const suffix = fileName.split(".").pop() || "";
   if (["js", "jsx"].includes(suffix)) return "javascript";
@@ -5,4 +10,32 @@ export const fileName2Language = (fileName: string) => {
   if (["json"].includes(suffix)) return "json";
   if (["css"].includes(suffix)) return "css";
   return "javascript";
+};
+
+export const compress = (data: string) => {
+  const buffer = strToU8(data);
+  const zipped = zlibSync(buffer, { level: 9 });
+  const binary = strFromU8(zipped, true);
+  return btoa(binary);
+};
+
+export const uncompress = (base64: string) => {
+  const binary = atob(base64);
+
+  const buffer = strToU8(binary, true);
+  const unzipped = unzlibSync(buffer);
+
+  return strFromU8(unzipped);
+};
+
+export const downloadFiles = async (files: Files) => {
+  const zip = new JSZip();
+
+  Object.keys(files).forEach((name) => {
+    zip.file(name, files[name].value);
+  });
+
+  const blob = await zip.generateAsync({ type: "blob" });
+
+  saveAs(blob, `code${Math.random().toString().slice(2, 8)}.zip`);
 };
