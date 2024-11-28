20324e5af633c6ef70c3a34ad4b36aa99a414b7a
liushuli <liushulialen@qq.com>
2024-11-28 19:05:25 +0800

    feat: 完成新增删除

--- react-playground/src/ReactPlayground/components/FileNameList/FileNameItem.tsx
+++ react-playground/src/ReactPlayground/components/FileNameList/FileNameItem.tsx
@@ -1,18 +1,51 @@
 import classnames from "classnames";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Popconfirm } from "antd";
 
 import styles from "./index.module.scss";
 
 export interface FileNameItemProps {
   value: string;
   actived: boolean;
+  creating: boolean;
+  readonly: boolean;
   onClick: () => void;
+  onEditComplete: (name: string) => void;
+  onRemove: () => void;
 }
 
 export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
-  const { value, actived = false, onClick } = props;
+  const {
+    value,
+    actived = false,
+    creating,
+    readonly,
+    onClick,
+    onEditComplete,
+    onRemove,
+  } = props;
 
   const [name, setName] = useState(value);
+  const [editing, setEditing] = useState(creating);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleDoubleClick = () => {
+    setEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const handleInputBlur = () => {
+    setEditing(false);
+    onEditComplete(name);
+  };
+
+  useEffect(() => {
+    if (creating) {
+      inputRef.current?.focus();
+    }
+  }, [creating]);
 
   return (
     <div
@@ -22,7 +55,37 @@ export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
       )}
       onClick={onClick}
     >
-      <span>{name}</span>
+      {editing ? (
+        <input
+          ref={inputRef}
+          className={styles["tabs-item-input"]}
+          value={name}
+          onBlur={handleInputBlur}
+          onChange={(e) => setName(e.target.value)}
+        />
+      ) : (
+        <>
+          <span onDoubleClick={handleDoubleClick}>{name}</span>
+          {!readonly ? (
+            <Popconfirm
+              title="确认删除该文件吗？"
+              okText="确定"
+              cancelText="取消"
+              onConfirm={(e) => {
+                e?.stopPropagation();
+                onRemove();
+              }}
+            >
+              <span style={{ marginLeft: 5, display: "flex" }}>
+                <svg width="12" height="12" viewBox="0 0 24 24">
+                  <line stroke="#999" x1="18" y1="6" x2="6" y2="18"></line>
+                  <line stroke="#999" x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </span>
+            </Popconfirm>
+          ) : null}
+        </>
+      )}
     </div>
   );
 };
--- react-playground/src/ReactPlayground/components/Preview/index.tsx
+++ react-playground/src/ReactPlayground/components/Preview/index.tsx
@@ -3,6 +3,14 @@ import { PlaygroundContext } from "../../PlaygroundContext";
 import { compile } from "./compiler";
 import iframeRaw from "./iframe.html?raw";
 import { IMPORT_MAP_FILE_NAME } from "../../files";
+import { Message } from "../Message";
+
+interface MessageData {
+  data: {
+    type: string;
+    message: string;
+  };
+}
 
 export default function Preview() {
   const { files } = useContext(PlaygroundContext);
@@ -34,12 +42,29 @@ export default function Preview() {
 
   const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
 
+  const [error, setError] = useState("");
+
+  const handleMessage = (msg: MessageData) => {
+    const { type, message } = msg.data;
+    if (type === "ERROR") {
+      setError(message);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("message", handleMessage);
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
   return (
     <div style={{ height: "100%" }}>
       <iframe
         src={iframeUrl}
         style={{ height: "100%", width: "100%", padding: 0, border: "none" }}
       />
+      <Message type="error" content={error} />
     </div>
   );
 }
