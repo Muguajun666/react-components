f6c162f89fe4cdb93f3c88fd7757138ebe2b284e
liushuli <liushulialen@qq.com>
2024-12-06 18:33:28 +0800

    feat: 新增低代码开发

---
+++ lowcode-editor/src/editor/components/EditArea/index.tsx
@@ -0,0 +1,37 @@
+import { useComponentConfigStore } from "../../stores/component-config";
+import { Component, useComponentsStore } from "../../stores/components";
+import React from "react";
+
+export function EditArea() {
+  const { components, addComponent } = useComponentsStore();
+  const { componentConfig } = useComponentConfigStore();
+
+  function renderComponents(components: Component[]): React.ReactNode {
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+
+      if (!config?.component) {
+        return null;
+      }
+
+      return React.createElement(
+        config.component,
+        {
+          key: component.id,
+          id: component.id,
+          name: component.name,
+          ...config.defaultProps,
+          ...component.props,
+        },
+        renderComponents(component.children || [])
+      );
+    });
+  }
+
+  return (
+    <div className="h-[100%]">
+      {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
+      {renderComponents(components)}
+    </div>
+  );
+}
