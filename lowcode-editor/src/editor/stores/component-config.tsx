713c7c33d6f1eb4e1c50c2362b2372a7f4eeeb06
liushuli <liushulialen@qq.com>
2024-12-11 15:32:53 +0800

    feat: 开发Form组件

--- lowcode-editor/src/editor/stores/component-config.tsx
+++ lowcode-editor/src/editor/stores/component-config.tsx
@@ -12,7 +12,9 @@ import TableDev from "../materials/Table/dev";
 import TableProd from "../materials/Table/prod";
 import TableColumnDev from "../materials/TableColumn/dev";
 import TableColumnProd from "../materials/TableColumn/prod";
-
+import FormDev from "../materials/Form/dev";
+import FormItemDev from "../materials/FormItem/dev";
+import FormItemProd from "../materials/FormItem/prod";
 export interface ComponentSetter {
   name: string;
   label: string;
@@ -188,6 +190,74 @@ export const useComponentConfigStore = create<State & Action>((set) => ({
       ],
       dev: TableColumnDev,
       prod: TableColumnProd,
+    },
+    Form: {
+      name: "Form",
+      defaultProps: {},
+      desc: "表单",
+      setter: [
+        {
+          name: "title",
+          label: "标题",
+          type: "input",
+        },
+      ],
+      events: [
+        {
+          name: "onFinish",
+          label: "提交事件",
+        },
+      ],
+      dev: FormDev,
+      prod: FormDev,
+    },
+    FormItem: {
+      name: "FormItem",
+      desc: "表单项",
+      defaultProps: {
+        name: new Date().getTime(),
+        label: "姓名",
+      },
+      setter: [
+        {
+          name: "type",
+          label: "类型",
+          type: "select",
+          options: [
+            {
+              label: "文本",
+              value: "input",
+            },
+            {
+              label: "日期",
+              value: "date",
+            },
+          ],
+        },
+        {
+          name: "label",
+          label: "标题",
+          type: "input",
+        },
+        {
+          name: "name",
+          label: "字段",
+          type: "input",
+        },
+        {
+          name: "rules",
+          label: "校验",
+          type: "select",
+          options: [
+            {
+              label: "必填",
+              value: "required",
+            },
+          ],
+        },
+      ],
+      dev: FormItemDev,
+      prod: FormItemProd,
     },
     Page: {
       name: "Page",
