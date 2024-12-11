08d1ce9b37bcf5e2ff66a7a6ba7391b8a0697a4c
liushuli <liushulialen@qq.com>
2024-12-11 14:31:11 +0800

    feat: 完成Table组件

--- lowcode-editor/src/editor/materials/Container/dev.tsx
+++ lowcode-editor/src/editor/materials/Container/dev.tsx
@@ -1,12 +1,33 @@
 import { CommonComponentProps } from "../../interface";
 import { useMaterailDrop } from "../../hooks/useMaterialDrop";
+import { useEffect, useRef } from "react";
+import { useDrag } from "react-dnd";
 
-const Container = ({ id, children, styles }: CommonComponentProps) => {
-  const { canDrop, drop } = useMaterailDrop(["Button", "Container"], id);
+const Container = ({ id, name, children, styles }: CommonComponentProps) => {
+  const { canDrop, drop } = useMaterailDrop(
+    ["Button", "Container", "Table"],
+    id
+  );
+
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const [_, drag] = useDrag({
+    type: name,
+    item: {
+      type: name,
+      dragType: "move",
+      id,
+    },
+  });
+
+  useEffect(() => {
+    drop(divRef);
+    drag(divRef);
+  }, []);
 
   return (
     <div
-      ref={drop}
+      ref={divRef}
       data-component-id={id}
       style={styles}
       className={`min-h-[100px] p-[20px] ${
