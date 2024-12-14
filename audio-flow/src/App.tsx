ea4b85a26f6cf16ff724ba3727a65b99cae43b65
liushuli <liushulialen@qq.com>
2024-12-15 03:30:03 +0800

    feat: 完成audio-flow

--- audio-flow/src/App.tsx
+++ audio-flow/src/App.tsx
@@ -4,25 +4,69 @@ import {
   BackgroundVariant,
   Connection,
   Controls,
+  Edge,
   MiniMap,
+  Panel,
   ReactFlow,
   useEdgesState,
   useNodesState,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import { OscillatorNode } from "./components/OscillatorNode";
+import { VolumeNode } from "./components/VolumeNode";
+import OutputNode from "./components/OutputNode";
+import { connect, createAudioNode, disconnect, removeAudioNode } from "./audio";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "a",
+    position: { x: 200, y: 0 },
+    data: { frequency: 220, type: "square" },
+    type: "osc",
+  },
+  {
+    id: "b",
+    position: { x: 150, y: 250 },
+    data: { gain: 0.5 },
+    type: "volume",
+  },
+  { id: "c", position: { x: 350, y: 400 }, data: {}, type: "out" },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges: Edge[] = [];
+
+const nodeTypes = {
+  osc: OscillatorNode,
+  volume: VolumeNode,
+  out: OutputNode,
+};
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = (params: Connection) => {
+    connect(params.source, params.target);
     setEdges((eds) => addEdge(params, eds));
+  };
+
+  const addOscNode = () => {
+    const id = Math.random().toString().substring(2, 8);
+    const position = { x: 0, y: 0 };
+    const type = "osc";
+    const data = { frequency: 400, type: "sine" };
+
+    setNodes([...nodes, { id, position, type, data }]);
+    createAudioNode(id, type, data);
+  };
+
+  const addVolumeNode = () => {
+    const id = Math.random().toString().substring(2, 8);
+    const position = { x: 0, y: 0 };
+    const type = "volume";
+    const data = { gain: 0.5 };
+
+    setNodes([...nodes, { id, position, type, data }]);
+    createAudioNode(id, type, data);
   };
 
   return (
@@ -33,11 +77,37 @@ export default function App() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        onNodesDelete={(nodes) => {
+          for (const { id } of nodes) {
+            removeAudioNode(id);
+          }
+        }}
+        onEdgesDelete={(edges) => {
+          for (const item of edges) {
+            const { source, target } = item;
+            disconnect(source, target);
+          }
+        }}
         fitView
       >
         <Controls />
         <MiniMap />
         <Background variant={BackgroundVariant.Lines} />
+        <Panel className={"space-x-4"} position="top-right">
+          <button
+            className={"p-[4px] rounded bg-white shadow"}
+            onClick={addOscNode}
+          >
+            添加振荡器节点
+          </button>
+          <button
+            className={"p-[4px] rounded bg-white shadow"}
+            onClick={addVolumeNode}
+          >
+            添加音量节点
+          </button>
+        </Panel>
       </ReactFlow>
     </div>
   );
---
+++ audio-flow/src/components/OutputNode/index.tsx
@@ -0,0 +1,31 @@
+import { Handle, Position } from "@xyflow/react";
+import { useState } from "react";
+import { toggleAudio } from "../../audio";
+
+const OutputNode = () => {
+  const [isRunning, setIsRunning] = useState(false);
+
+  return (
+    <div className={"bg-white shadow-xl p-[20px]"}>
+      <Handle
+        type="target"
+        className="w-[10px] h-[10px]"
+        position={Position.Top}
+      />
+
+      <div>
+        <p>输出节点</p>
+        <button
+          onClick={() => {
+            setIsRunning((isRunning) => !isRunning);
+            toggleAudio();
+          }}
+        >
+          {isRunning ? <span role="img">🔈</span> : <span role="img">🔇</span>}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OutputNode;
